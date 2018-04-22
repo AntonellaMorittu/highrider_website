75e9edb32b6ea670b493641cdcd16e9e1ec98810
Antonella Morittu <morittuantonella@gmail.com>
2018-04-22 14:11:11 +0200

    add link_to homepage

--- config.rb
+++ config.rb
@@ -35,7 +35,8 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :directory_indexes
+  activate :minify_css
+  activate :minify_javascript
+end
